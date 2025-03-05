5aaf3f61ec2736d257e79c29559e53ac1e39ee32
Raghav panthi <159898851+Mrcoderv@users.noreply.github.com>
2025-03-05 18:34:25 +0545

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['placeholder.svg'],
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+  output: 'export',
+  basePath: '',
+  trailingSlash: true,
+};
+
+export default nextConfig;
+
